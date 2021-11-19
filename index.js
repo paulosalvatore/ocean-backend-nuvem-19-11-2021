1c7ceae52c4a1a3494cc2c494bcf5959a2cd8515
Paulo Salvatore <salvatoregames@gmail.com>
2021-11-19 11:22:42 -0300

    Conexão com MongoDB Cloud Atlas

--- index.js
+++ index.js
@@ -7,19 +7,15 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
-const dbName = "ocean_bancodados_18_11_2021";
+const url = "mongodb+srv://admin:1b7yo0CUPxyYfFEk@cluster0.749ln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const dbName = "ocean_nuvem_19_11_2021";
 
 async function main() {
-    /*
     const client = await MongoClient.connect(url);
 
     const db = client.db(dbName);
 
     const collection = db.collection("herois");
-    */
-
-    const collection = undefined;
 
     const app = express();
 
