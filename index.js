7a669d1419f2ca3b4fdb834e9468acbe6fdf7d76
Taiz <62393795+TaizAlves@users.noreply.github.com>
2021-11-19 11:24:39 -0300

    configuracao back mongodbcloud

--- index.js
+++ index.js
@@ -7,19 +7,18 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
-const dbName = "ocean_bancodados_18_11_2021";
+const url = "mongodb+srv://taiz:taiz@cluster0.uei3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority;
+const dbName = "ocean-backend-nuvem-19-11-2021";
 
 async function main() {
 
-    /*
+    
     const client = await MongoClient.connect(url);
 
     const db = client.db(dbName);
 
     const collection = db.collection("herois");
-    */
-    const collection = undefined;
+    
 
     const app = express();
 
